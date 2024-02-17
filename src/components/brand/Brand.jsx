ea4eaa21b71aacbc9c012dd1a2cde5533c363761
brandmoredev <brandmoredev@gmail.com>
2024-02-17 17:38:00 +0300

    Updates whatAI container

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import './Brand.css'
+import './Brand.css';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className='ai__brand section__padding'>
+      <div className="ai__brand-item">
+        <img src={google} alt="google" />
+      </div>
+      <div className="ai__brand-item">
+        <img src={slack} alt="slack" />
+      </div>
+      <div className="ai__brand-item">
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div className="ai__brand-item">
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div className="ai__brand-item">
+        <img src={shopify} alt="shopify"/>
+      </div>
     </div>
   )
 }
