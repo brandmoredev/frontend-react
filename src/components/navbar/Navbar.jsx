cd4ed3ab7a6369e1dd1925c43a40aab754e1f48c
brandmoredev <brandmoredev@gmail.com>
2024-02-24 11:45:47 +0300

    Updates content

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Menu = () => {
   return(
     <>
       <p><a href="#home">Home</a></p>
-      <p><a href="#whatAI">What is AI</a></p>
+      <p><a href="#whatAI">What is GPT3</a></p>
       <p><a href="#possibility">Open AI</a></p>
       <p><a href="#features">Case Studies</a></p>
       <p><a href="#blog">Library</a></p>
--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -4,7 +4,7 @@ import { Feature } from '../../components';
 
 const Features = () => {
   return (
-    <div className='ai__features section__padding'>
+    <div className='ai__features section__padding' id='features'>
       <div className="ai__features-header">
         <h1 className='gradient__text'>
           The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.
@@ -13,20 +13,20 @@ const Features = () => {
       </div>
       <div className="ai__features__container">
         <Feature
-          title="Improving end distrusts instantly"
-          text="From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
+          title="Creative Project Catalyst"
+          text="Unleash boundless creativity with GPT-3 as your project companion. Explore innovative ideas and witness your imagination come to life."
         />
         <Feature
-          title="Become the tended active"
-          text="Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to."
+          title="Conversational Intelligence"
+          text="Transform interactions with intelligent chatbots powered by GPT-3. Elevate conversations to new heights, making them natural, dynamic, and engaging."
         />
         <Feature
-          title="Message or am nothing"
-          text="Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+          title="Knowledge Amplification in Education"
+          text="Revolutionize learning with GPT-3's educational prowess. From beginner primers to advanced insights, amplify your knowledge and skills in artificial intelligence."
         />
         <Feature
-          title="Really boy law county"
-          text="Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush."
+          title="Multifaceted Problem-Solving"
+          text="Experience GPT-3's diverse applications across industries. From creative brainstorming to innovative problem-solving, witness its impact in pushing boundaries."
         />
       </div>
     </div>
